356e16128b9877b5e29ee63683d79e0baee23923
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 22:03:31 +0530

    commited by n8n

---
+++ 993645e99b6848769804926fce977077-folder-structure/backend/models/category.py
@@ -0,0 +1,7 @@
+from backend.database import db
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    parent_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
+    children = db.relationship("Category")
