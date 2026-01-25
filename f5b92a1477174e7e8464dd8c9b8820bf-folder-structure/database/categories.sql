f9f65fbbc568180c19d454cb3ed84bf54cc1cedd
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 22:33:20 +0530

    commited by n8n

--- f5b92a1477174e7e8464dd8c9b8820bf-folder-structure/database/categories.sql
+++ f5b92a1477174e7e8464dd8c9b8820bf-folder-structure/database/categories.sql
@@ -2,5 +2,5 @@ CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(120) NOT NULL,
     parent_id INT,
-    FOREIGN KEY (parent_id) REFERENCES categories(id) ON DELETE SET NULL
+    FOREIGN KEY (parent_id) REFERENCES categories(id) ON DELETE CASCADE
 );
