52983a8c9c7110ff0f3a07dc63a88ad1b3a1b255
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 22:04:54 +0530

    commited by n8n

--- 993645e99b6848769804926fce977077-folder-structure/backend/models/product.py
+++ 993645e99b6848769804926fce977077-folder-structure/backend/models/product.py
@@ -5,4 +5,3 @@ class Product(db.Model):
     name = db.Column(db.String(120), unique=True, nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=False)
-    category = db.Column(db.String(120), nullable=False)
