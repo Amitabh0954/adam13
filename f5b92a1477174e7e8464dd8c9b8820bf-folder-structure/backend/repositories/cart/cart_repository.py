123c66349104f774e2198c0cd571c3053db712a5
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 22:27:53 +0530

    commited by n8n

---
+++ f5b92a1477174e7e8464dd8c9b8820bf-folder-structure/backend/repositories/cart/cart_repository.py
@@ -0,0 +1,27 @@
+import logging
+from backend.models.cart import Cart, CartItem
+from backend.database import db
+
+logger = logging.getLogger(__name__)
+
+class CartRepository:
+    
+    def get_cart(self, user_id: int) -> Cart:
+        return Cart.query.filter_by(user_id=user_id).first()
+
+    def add_to_cart(self, user_id: int, product_id: int, quantity: int) -> Cart:
+        cart = self.get_cart(user_id)
+        if not cart:
+            cart = Cart(user_id=user_id)
+            db.session.add(cart)
+            db.session.commit()
+        
+        cart_item = CartItem.query.filter_by(cart_id=cart.id, product_id=product_id).first()
+        if cart_item:
+            cart_item.quantity += quantity
+        else:
+            cart_item = CartItem(cart_id=cart.id, product_id=product_id, quantity=quantity)
+            db.session.add(cart_item)
+        
+        db.session.commit()
+        return cart
