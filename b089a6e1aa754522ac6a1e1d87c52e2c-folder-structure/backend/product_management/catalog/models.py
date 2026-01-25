cc63171d56c52e29042a42c0f75fa922cc169022
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 20:43:26 +0530

    commited by n8n

---
+++ b089a6e1aa754522ac6a1e1d87c52e2c-folder-structure/backend/product_management/catalog/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float, Sequence
+
+db = SQLAlchemy()
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = Column(Integer, Sequence('product_id_seq'), primary_key=True)
+    name = Column(String(120), unique=True, nullable=False)
+    description = Column(String(500), nullable=False)
+    price = Column(Float, nullable=False)
+
+    def __repr__(self) -> str:
+        return f'<Product {self.name}>'
