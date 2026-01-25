d49b4a0fdba93c94fa6f16d21463bba6af107e03
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 22:29:16 +0530

    commited by n8n

--- f5b92a1477174e7e8464dd8c9b8820bf-folder-structure/backend/models/product.py
+++ f5b92a1477174e7e8464dd8c9b8820bf-folder-structure/backend/models/product.py
@@ -1,7 +1,7 @@
 from backend.database import db
 
 class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary key=True)
     name = db.Column(db.String(120), nullable=False, unique=True)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=False)
