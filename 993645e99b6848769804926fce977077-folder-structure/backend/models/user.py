d4c5e25ad60cf2cc1629c4ed662d2a10172f863b
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 22:17:55 +0530

    commited by n8n

--- 993645e99b6848769804926fce977077-folder-structure/backend/models/user.py
+++ 993645e99b6848769804926fce977077-folder-structure/backend/models/user.py
@@ -1,11 +1,7 @@
 from backend.database import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    login_attempts = db.Column(db.Integer, default=0)
-
-    def __init__(self, email: str, password: str):
-        self.email = email
-        self.password = password
+    password = db.Column(db.String(60), nullable=False)
