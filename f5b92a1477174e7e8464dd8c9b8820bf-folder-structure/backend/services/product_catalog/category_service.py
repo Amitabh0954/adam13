5a8f111f61a0f87937c2b9079e44d44c4804c394
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 22:33:18 +0530

    commited by n8n

--- f5b92a1477174e7e8464dd8c9b8820bf-folder-structure/backend/services/product_catalog/category_service.py
+++ f5b92a1477174e7e8464dd8c9b8820bf-folder-structure/backend/services/product_catalog/category_service.py
@@ -1,12 +1,12 @@
 from backend.repositories.product.category_repository import CategoryRepository
 
 class CategoryService:
-    
+
     def __init__(self):
         self.category_repository = CategoryRepository()
 
     def add_category(self, name: str, parent_id: int = None):
         return self.category_repository.add_category(name, parent_id)
 
-    def get_all_categories(self):
-        return self.category_repository.get_all_categories()
+    def update_category(self, category_id: int, name: str = None, parent_id: int = None):
+        return self.category_repository.update_category(category_id, name, parent_id)
