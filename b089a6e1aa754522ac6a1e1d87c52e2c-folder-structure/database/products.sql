3832b18b4e739cf27789c16fd82b2c1c8892dd29
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 22:20:59 +0530

    Delete b089a6e1aa754522ac6a1e1d87c52e2c-folder-structure directory

--- b089a6e1aa754522ac6a1e1d87c52e2c-folder-structure/database/products.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE products (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(120) NOT NULL UNIQUE,
-    description VARCHAR(500) NOT NULL,
-    price FLOAT NOT NULL CHECK (price > 0),
-    category VARCHAR(50) NULL,
-    attributes VARCHAR(500) NULL,   -- Combined attributes as a JSON string
-    is_deleted BOOLEAN NOT NULL DEFAULT FALSE
-);
