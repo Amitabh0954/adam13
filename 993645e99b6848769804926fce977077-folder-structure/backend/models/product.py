69faf0de83e6eb0e32b8f50595578abf7d1365b3
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-25 22:04:49 +0530

    commited by n8n

--- 993645e99b6848769804926fce977077-folder-structure/backend/models/product.py
+++ 993645e99b6848769804926fce977077-folder-structure/backend/models/product.py
@@ -5,3 +5,5 @@ class Product(db.Model):
     name = db.Column(db.String(120), unique=True, nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship("Category")
