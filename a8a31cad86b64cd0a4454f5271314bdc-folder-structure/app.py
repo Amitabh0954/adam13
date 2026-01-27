59fb3ed71cfe94f692d0635138ffcb4b8d71e719
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 15:50:22 +0530

    commited by n8n

--- a8a31cad86b64cd0a4454f5271314bdc-folder-structure/app.py
+++ a8a31cad86b64cd0a4454f5271314bdc-folder-structure/app.py
@@ -1,6 +1,6 @@
 # Epic Title: User Account Management
 
-from flask import Flask
+from flask import Flask, session
 from backend.user_account_management.controllers.user_controller import user_bp
 from backend.database import engine, Base
 
@@ -12,6 +12,11 @@ app.register_blueprint(user_bp, url_prefix='/users')
 
 # Create tables
 Base.metadata.create_all(bind=engine)
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = 1800  # 30 minutes
 
 if __name__ == '__main__':
     app.run(debug=True)
