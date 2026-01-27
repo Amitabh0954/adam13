ef8fd51d8c8955434b2e710e70d5708bc2ce499f
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 12:00:07 +0530

    commited by n8n

--- 5c2463f50f614472bd8c9cf0f4e7baeb-folder-structure/backend/repositories/authentication/user_repository.py
+++ 5c2463f50f614472bd8c9cf0f4e7baeb-folder-structure/backend/repositories/authentication/user_repository.py
@@ -5,6 +5,9 @@ class UserRepository:
     def find_by_email(self, email: str) -> User:
         return User.query.filter_by(email=email).first()
     
+    def find_by_id(self, user_id: int) -> User:
+        return User.query.get(user_id)
+    
     def save_user(self, user: User):
         db.session.add(user)
         db.session.commit()
