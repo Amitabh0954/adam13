ac93189fe389632456db8942eb1eb64ba3e39e97
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 12:53:02 +0530

    commited by n8n

--- 37c13091b1ac485daae6b5de995cc084-folder-structure/backend/models/user.py
+++ 37c13091b1ac485daae6b5de995cc084-folder-structure/backend/models/user.py
@@ -5,3 +5,11 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    preferences = db.Column(db.JSON, nullable=True)
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "email": self.email,
+            "preferences": self.preferences
+        }
