de621464a92a680cc83814a7e2be5465b8e32b8f
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 12:53:42 +0530

    commited by n8n

---
+++ 37c13091b1ac485daae6b5de995cc084-folder-structure/backend/models/product.py
@@ -0,0 +1,15 @@
+from backend.extensions import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "description": self.description
+        }
