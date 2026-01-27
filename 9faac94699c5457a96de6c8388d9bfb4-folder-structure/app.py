cc7c454088d9a1a4ae62d6166f3fa67016781fa1
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 14:45:26 +0530

    commited by n8n

--- 9faac94699c5457a96de6c8388d9bfb4-folder-structure/app.py
+++ 9faac94699c5457a96de6c8388d9bfb4-folder-structure/app.py
@@ -4,7 +4,6 @@ from flask import Flask
 from backend.controllers.product_catalog.product_controller import product_bp
 from backend.controllers.product_catalog.product_update_controller import product_update_bp
 from backend.controllers.product_catalog.product_delete_controller import product_delete_bp
-from backend.controllers.product_catalog.product_search_controller import product_search_bp
 from backend.database import engine, Base
 
 app = Flask(__name__)
@@ -14,7 +13,6 @@ app.secret_key = 'supersecretkey'  # Choose a secure key for production
 app.register_blueprint(product_bp, url_prefix='/catalog')
 app.register_blueprint(product_update_bp, url_prefix='/catalog')
 app.register_blueprint(product_delete_bp, url_prefix='/catalog')
-app.register_blueprint(product_search_bp, url_prefix='/catalog')
 
 # Create tables
 Base.metadata.create_all(bind=engine)
