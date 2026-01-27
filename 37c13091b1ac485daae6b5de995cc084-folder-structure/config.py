28d0437797684ab8cc4919dba1dfee943f03e94f
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 12:52:18 +0530

    commited by n8n

--- 37c13091b1ac485daae6b5de995cc084-folder-structure/config.py
+++ 37c13091b1ac485daae6b5de995cc084-folder-structure/config.py
@@ -5,10 +5,19 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "mysql+pymysql://user:password@localhost/dbname"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # Email settings
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.example.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+
     # Session settings
     SESSION_TYPE = 'filesystem'
     SESSION_PERMANENT = False
-    PERMANENT_SESSION_LIFETIME = int(os.environ.get("PERMANENT_SESSION_LIFETIME", 1800))  # 30 minutes of inactivity
 
     # Application settings
     REMEMBER_COOKIE_DURATION = 3600  # 1 hour
+    PERMANENT_SESSION_LIFETIME = 1800  # 30 minutes of inactivity
