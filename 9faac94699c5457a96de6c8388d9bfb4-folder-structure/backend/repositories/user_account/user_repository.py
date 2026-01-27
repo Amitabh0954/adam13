f729b5b8a343cb9171493a4e350ff5583b2558d8
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 14:44:57 +0530

    commited by n8n

--- 9faac94699c5457a96de6c8388d9bfb4-folder-structure/backend/repositories/user_account/user_repository.py
+++ 9faac94699c5457a96de6c8388d9bfb4-folder-structure/backend/repositories/user_account/user_repository.py
@@ -1,14 +1,15 @@
-# Epic Title: User Account Management
+# Epic Title: Shopping Cart Functionality
 
 from backend.database import db_session
 from backend.models.user import User
 
 class UserRepository:
-    def get_user_by_email(self, email: str) -> User:
-        return db_session.query(User).filter_by(email=email).first()
+    def get_user_by_id(self, user_id: int) -> User:
+        return db_session.query(User).filter_by(id=user_id).first()
 
-    def create_user(self, email: str, password: str) -> User:
-        new_user = User(email=email, password=password)
-        db_session.add(new_user)
-        db_session.commit()
-        return new_user
+    def update_user_cart(self, user_id: int, cart: dict):
+        user = self.get_user_by_id(user_id)
+        if user:
+            user.cart = cart
+            db_session.add(user)
+            db_session.commit()
