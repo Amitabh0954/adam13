e5147a662259d83a5917567825476e6ae0d48a11
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 15:27:17 +0530

    commited by n8n

--- e36e6768693243e7a46d2028a2f7973d-folder-structure/app.py
+++ e36e6768693243e7a46d2028a2f7973d-folder-structure/app.py
@@ -2,7 +2,6 @@
 
 from flask import Flask
 from backend.controllers.product_controller import product_bp
-from backend.controllers.category_controller import category_bp
 from backend.database import engine, Base
 
 app = Flask(__name__)
@@ -10,7 +9,6 @@ app.secret_key = 'supersecretkey'  # Choose a secure key for production
 
 # Register Blueprints
 app.register_blueprint(product_bp, url_prefix='/products')
-app.register_blueprint(category_bp, url_prefix='/categories')
 
 # Create tables
 Base.metadata.create_all(bind=engine)
