4266ec259525a845905251084ea6c47f60a6f6a7
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 11:45:59 +0530

    commited by n8n

---
+++ 0f1b5a6fd7194073930b097ddabb2324-folder-structure/backend/auth/models/user.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from backend.auth.extensions import db
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
