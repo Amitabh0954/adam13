15c68dfccb199213cb5eccb960b4979767c7ad19
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 11:22:49 +0530

    commited by n8n

---
+++ 7391c198f8004e3f96426d606fce4d69-folder-structure/backend/repositories/user_account/user_repository.py
@@ -0,0 +1,12 @@
+from user_account_management.models import User
+from user_account_management.extensions import db
+
+class UserRepository:
+    def get_user_by_email(self, email: str) -> User:
+        return User.query.filter_by(email=email).first()
+
+    def create_user(self, email: str, password: str, first_name: str, last_name: str) -> User:
+        new_user = User(email=email, password=password, first_name=first_name, last_name=last_name)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
