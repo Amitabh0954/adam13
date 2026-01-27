330978af2b6d15e87c450bb9d465c01f423e92cf
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 11:32:45 +0530

    commited by n8n

--- 7391c198f8004e3f96426d606fce4d69-folder-structure/user_account_management/models.py
+++ 7391c198f8004e3f96426d606fce4d69-folder-structure/user_account_management/models.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 import datetime
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary key=True)
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     last_login_at = db.Column(db.DateTime, nullable=True)
@@ -15,9 +15,9 @@ class User(db.Model, UserMixin):
     invalid_login_attempts = db.Column(db.Integer, default=0, nullable=False)
 
 class PasswordResetToken(db.Model):
-    id = db.Column(db.Integer, primary key=True)
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('reset_tokens', lazy=True))
+    user = db.relationship('User', backref=db.backref('reset_tokens', lazy=True)
     token = db.Column(db.String(100), nullable=False, unique=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
     expires_at = db.Column(db.DateTime, nullable=False)
