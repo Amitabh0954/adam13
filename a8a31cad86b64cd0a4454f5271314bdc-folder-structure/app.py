a8656f327b1f98952d380cb350c046d4253cd7f0
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 15:55:17 +0530

    commited by n8n

--- a8a31cad86b64cd0a4454f5271314bdc-folder-structure/app.py
+++ a8a31cad86b64cd0a4454f5271314bdc-folder-structure/app.py
@@ -3,7 +3,6 @@
 from flask import Flask
 from backend.user_account_management.controllers.user_controller import user_bp
 from backend.user_account_management.controllers.password_reset_controller import password_reset_bp
-from backend.user_account_management.controllers.profile_controller import profile_bp
 from backend.database import engine, Base
 
 app = Flask(__name__)
@@ -12,7 +11,6 @@ app.secret_key = 'supersecretkey'  # Choose a secure key for production
 # Register Blueprints
 app.register_blueprint(user_bp, url_prefix='/users')
 app.register_blueprint(password_reset_bp, url_prefix='/auth')
-app.register_blueprint(profile_bp, url_prefix='/profile')
 
 # Create tables
 Base.metadata.create_all(bind=engine)
