55747046f020a0698f1e6a4b9754201383bb797a
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 11:30:41 +0530

    commited by n8n

--- 7391c198f8004e3f96426d606fce4d69-folder-structure/product_catalog_management/models.py
+++ 7391c198f8004e3f96426d606fce4d69-folder-structure/product_catalog_management/models.py
@@ -17,5 +17,5 @@ class Category(db.Model):
 
 product_category = db.Table('product_category',
     db.Column('product_id', db.Integer, db.ForeignKey('product.id'), primary_key=True),
-    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True)
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary key=True)
 )
