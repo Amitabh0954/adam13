6d399c56d5b2a64ea9aa0cd1320c19da41dc25ef
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 14:40:02 +0530

    commited by n8n

---
+++ 9faac94699c5457a96de6c8388d9bfb4-folder-structure/database/1create_users_table.sql
@@ -0,0 +1,7 @@
+# Epic Title: User Account Management
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(120) NOT NULL UNIQUE,
+    password VARCHAR(128) NOT NULL
+);
