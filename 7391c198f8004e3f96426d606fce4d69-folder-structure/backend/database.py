c31715202e6a992b524d20328284de72db88e0fe
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 14:38:51 +0530

    commited by n8n

---
+++ 7391c198f8004e3f96426d606fce4d69-folder-structure/backend/database.py
@@ -0,0 +1,19 @@
+# Epic Title: User Account Management
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URI = 'mysql+pymysql://user:password@localhost/db_name'
+
+engine = create_engine(DATABASE_URI)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Dependency for sessions
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
