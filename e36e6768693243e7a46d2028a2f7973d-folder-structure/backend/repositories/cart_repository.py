3a6faf54c97006c42eeba8ba58946bf42ccf3c20
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 15:24:58 +0530

    commited by n8n

---
+++ e36e6768693243e7a46d2028a2f7973d-folder-structure/backend/repositories/cart_repository.py
@@ -0,0 +1,27 @@
+# Epic Title: Shopping Cart Functionality
+
+from backend.database import db_session
+from backend.models.cart import Cart, CartItem
+from typing import Optional
+
+class CartRepository:
+    def get_cart_by_user(self, user_id: int) -> Optional[Cart]:
+        return db_session.query(Cart).filter_by(user_id=user_id).first()
+
+    def get_cart_by_session(self) -> Optional[Cart]:
+        return db_session.query(Cart).filter_by(user_id=None).first()
+
+    def create_cart(self, user_id: Optional[int]) -> Cart:
+        new_cart = Cart(user_id=user_id)
+        db_session.add(new_cart)
+        db_session.commit()
+        return new_cart
+
+    def add_product_to_cart(self, cart: Cart, product_id: int, quantity: int):
+        cart_item = db_session.query(CartItem).filter_by(cart_id=cart.id, product_id=product_id).first()
+        if cart_item:
+            cart_item.quantity += quantity
+        else:
+            cart_item = CartItem(cart_id=cart.id, product_id=product_id, quantity=quantity)
+            db_session.add(cart_item)
+        db_session.commit()
