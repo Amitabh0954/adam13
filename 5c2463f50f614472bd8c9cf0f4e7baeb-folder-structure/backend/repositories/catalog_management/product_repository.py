dd07c7c6fbd540153752b7eb932e2da6f7a867fb
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 12:01:58 +0530

    commited by n8n

--- 5c2463f50f614472bd8c9cf0f4e7baeb-folder-structure/backend/repositories/catalog_management/product_repository.py
+++ 5c2463f50f614472bd8c9cf0f4e7baeb-folder-structure/backend/repositories/catalog_management/product_repository.py
@@ -5,6 +5,12 @@ class ProductRepository:
     def find_by_name(self, name: str) -> Product:
         return Product.query.filter_by(name=name).first()
     
+    def find_by_id(self, product_id: int) -> Product:
+        return Product.query.get(product_id)
+    
     def save_product(self, product: Product):
         db.session.add(product)
+        db.session.commit()
+
+    def update_product(self, product: Product):
         db.session.commit()
