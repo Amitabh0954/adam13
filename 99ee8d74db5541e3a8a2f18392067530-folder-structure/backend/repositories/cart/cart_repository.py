28241ba39b29a87214b25af9616af43cb58bd814
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 14:54:52 +0530

    commited by n8n

---
+++ 99ee8d74db5541e3a8a2f18392067530-folder-structure/backend/repositories/cart/cart_repository.py
@@ -0,0 +1,42 @@
+# Epic Title: Shopping Cart Functionality
+
+from backend.database import db_session
+from backend.models.cart import Cart, CartItem
+
+class CartRepository:
+    def add_to_cart(self, user_id: int, product_id: int, quantity: int):
+        cart = db_session.query(Cart).filter_by(user_id=user_id).first()
+        if not cart:
+            cart = Cart(user_id=user_id)
+            db_session.add(cart)
+            db_session.commit()
+        
+        cart_item = db_session.query(CartItem).filter_by(cart_id=cart.id, product_id=product_id).first()
+        if cart_item:
+            cart_item.quantity += quantity
+        else:
+            cart_item = CartItem(cart_id=cart.id, product_id=product_id, quantity=quantity)
+
+        db_session.add(cart_item)
+        db_session.commit()
+
+    def get_cart(self, user_id: int) -> Dict[str, Any]:
+        cart = db_session.query(Cart).filter_by(user_id=user_id).first()
+        if not cart:
+            return {}
+
+        return {
+            'cart_id': cart.id,
+            'user_id': cart.user_id,
+            'items': [{'product_id': item.product_id, 'quantity': item.quantity} for item in cart.items]
+        }
+
+    def remove_from_cart(self, user_id: int, product_id: int):
+        cart = db_session.query(Cart).filter_by(user_id=user_id).first()
+        if not cart:
+            return
+        
+        cart_item = db_session.query(CartItem).filter_by(cart_id=cart.id, product_id=product_id).first()
+        if cart_item:
+            db_session.delete(cart_item)
+            db_session.commit()
