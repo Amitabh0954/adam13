b001874a1eddb15754f44b73d270cc3de55107b3
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 14:27:04 +0530

    commited by n8n

--- 45f7ad63c86c4587872a7377bcaf4ffa-folder-structure/database/1create_users_table.sql
+++ 45f7ad63c86c4587872a7377bcaf4ffa-folder-structure/database/1create_users_table.sql
@@ -3,5 +3,6 @@
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(120) NOT NULL UNIQUE,
-    password VARCHAR(128) NOT NULL
+    password VARCHAR(128) NOT NULL,
+    preferences TEXT
 );
