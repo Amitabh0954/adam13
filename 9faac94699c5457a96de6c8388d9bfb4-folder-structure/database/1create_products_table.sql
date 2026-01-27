f9c3f2794161ed9be4f7ad7939928545de5a6e6e
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 14:45:24 +0530

    commited by n8n

--- 9faac94699c5457a96de6c8388d9bfb4-folder-structure/database/1create_products_table.sql
+++ 9faac94699c5457a96de6c8388d9bfb4-folder-structure/database/1create_products_table.sql
@@ -4,6 +4,5 @@ CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     description VARCHAR(255) NOT NULL,
-    price FLOAT NOT NULL CHECK (price > 0),
-    category VARCHAR(255) NOT NULL
+    price FLOAT NOT NULL CHECK (price > 0)
 );
