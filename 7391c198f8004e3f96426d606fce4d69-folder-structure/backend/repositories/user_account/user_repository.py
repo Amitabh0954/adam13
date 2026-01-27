f88fbca8a2e4f6b9e3f27ce918fca56b72db47da
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 11:32:48 +0530

    commited by n8n

--- 7391c198f8004e3f96426d606fce4d69-folder-structure/backend/repositories/user_account/user_repository.py
+++ 7391c198f8004e3f96426d606fce4d69-folder-structure/backend/repositories/user_account/user_repository.py
@@ -1,5 +1,6 @@
-from user_account_management.models import User
+from user_account_management.models import User, PasswordResetToken
 from user_account_management.extensions import db
+from datetime import datetime
 
 class UserRepository:
     def get_user_by_email(self, email: str) -> User:
@@ -10,6 +11,19 @@ class UserRepository:
         db.session.add(new_user)
         db.session.commit()
         return new_user
-        
+
     def update_user(self, user: User) -> None:
+        db.session.commit()
+    
+    def create_password_reset_token(self, user: User, token: str, expires_at: datetime) -> PasswordResetToken:
+        password_reset_token = PasswordResetToken(user_id=user.id, token=token, expires_at=expires_at)
+        db.session.add(password_reset_token)
+        db.session.commit()
+        return password_reset_token
+
+    def get_password_reset_token_by_token(self, token: str) -> PasswordResetToken:
+        return PasswordResetToken.query.filter_by(token=token).first()
+
+    def delete_password_reset_token(self, password_reset_token: PasswordResetToken) -> None:
+        db.session.delete(password_reset_token)
         db.session.commit()
