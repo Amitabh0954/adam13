721b2c53edc41a7a162c6e21fa55f76c4dd31deb
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 11:46:54 +0530

    commited by n8n

--- 0f1b5a6fd7194073930b097ddabb2324-folder-structure/backend/auth/models/user.py
+++ 0f1b5a6fd7194073930b097ddabb2324-folder-structure/backend/auth/models/user.py
@@ -11,6 +11,14 @@ class User(db.Model, UserMixin):
     is_active = db.Column(db.Boolean, default=True)
     invalid_login_attempts = db.Column(db.Integer, default=0)
 
+class PasswordResetToken(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('reset_tokens', lazy=True))
+    token = db.Column(db.String(100), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    expires_at = db.Column(db.DateTime, nullable=False)
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
