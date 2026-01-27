e62eae40b798c88c55ace44a243d17a46c4b238d
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 11:47:48 +0530

    commited by n8n

---
+++ 0f1b5a6fd7194073930b097ddabb2324-folder-structure/backend/products/models/product.py
@@ -0,0 +1,15 @@
+from backend.auth.extensions import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "description": self.description
+        }
