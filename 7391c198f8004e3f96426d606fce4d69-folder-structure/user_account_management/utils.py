a5b1d82c7d89dcddcedfa272baae25216604d6f0
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 11:32:47 +0530

    commited by n8n

--- 7391c198f8004e3f96426d606fce4d69-folder-structure/user_account_management/utils.py
+++ 7391c198f8004e3f96426d606fce4d69-folder-structure/user_account_management/utils.py
@@ -1,4 +1,6 @@
 from .models import db, User
+from flask_mail import Message
+from .extensions import mail
 
 def setup_database():
     db.create_all()
@@ -7,3 +9,9 @@ def validate_password(password: str) -> bool:
     if len(password) < 8 or not any(char.isdigit() for char in password) or not any(char.isalpha() for char in password):
         return False
     return True
+
+def send_email(subject: str, recipients: list, text_body: str, html_body: str):
+    msg = Message(subject, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    mail.send(msg)
