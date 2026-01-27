e04da03217368c0b2fb798ceaadb755d6b8d69bb
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 15:27:11 +0530

    commited by n8n

--- e36e6768693243e7a46d2028a2f7973d-folder-structure/database/1create_categories_table.sql
+++ e36e6768693243e7a46d2028a2f7973d-folder-structure/database/1create_categories_table.sql
@@ -2,5 +2,7 @@
 
 CREATE TABLE categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE
+    name VARCHAR(255) NOT NULL UNIQUE,
+    parent_id INT,
+    FOREIGN KEY (parent_id) REFERENCES categories(id) ON DELETE SET NULL
 );
