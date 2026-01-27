8ec25d570cb7315fc9077bccf1a0bb42ad8148b8
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 11:19:24 +0530

    commited by n8n

--- 7391c198f8004e3f96426d606fce4d69-folder-structure/config.py
+++ 7391c198f8004e3f96426d606fce4d69-folder-structure/config.py
@@ -4,3 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "a_temporary_secret_key"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "mysql+pymysql://user:password@localhost/dbname"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Session and security settings
+    REMEMBER_COOKIE_DURATION = 3600  # 1 hour
+    PERMANENT_SESSION_LIFETIME = 1800  # 30 minutes of inactivity
