07e507f755d477b99025faf87e700c5c52e80b25
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 15:54:41 +0530

    commited by n8n

--- a8a31cad86b64cd0a4454f5271314bdc-folder-structure/backend/shopping_cart_functionality/repositories/cart_repository.py
+++ a8a31cad86b64cd0a4454f5271314bdc-folder-structure/backend/shopping_cart_functionality/repositories/cart_repository.py
@@ -2,7 +2,6 @@
 
 from backend.database import db_session
 from backend.shopping_cart_functionality.models.cart import Cart
-from typing import List
 
 class CartRepository:
     def add_to_cart(self, user_id: int, product_id: int, quantity: int):
@@ -20,13 +19,3 @@ class CartRepository:
         if cart_item:
             db_session.delete(cart_item)
             db_session.commit()
-    
-    def update_cart(self, user_id: int, product_id: int, quantity: int):
-        cart_item = db_session.query(Cart).filter_by(user_id=user_id, product_id=product_id).first()
-        if cart_item:
-            cart_item.quantity = quantity
-            db_session.add(cart_item)
-            db_session.commit()
-    
-    def get_cart_by_user(self, user_id: int) -> List[Cart]:
-        return db_session.query(Cart).filter_by(user_id=user_id).all()
