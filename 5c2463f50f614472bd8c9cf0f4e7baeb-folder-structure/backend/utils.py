214c5cdaec6c0e84824ba5a18a1b7518affb8782
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 12:00:06 +0530

    commited by n8n

--- 5c2463f50f614472bd8c9cf0f4e7baeb-folder-structure/backend/utils.py
+++ 5c2463f50f614472bd8c9cf0f4e7baeb-folder-structure/backend/utils.py
@@ -2,3 +2,11 @@ from .extensions import db
 
 def setup_database():
     db.create_all()
+
+def send_email(subject: str, recipient: str, body: str):
+    from flask_mail import Message
+    from .extensions import mail
+
+    msg = Message(subject, recipients=[recipient])
+    msg.body = body
+    mail.send(msg)
