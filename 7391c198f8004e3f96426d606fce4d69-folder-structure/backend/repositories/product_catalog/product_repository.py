8313321840a299c3398f4f3992ee424994b029bb
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 11:32:18 +0530

    commited by n8n

--- 7391c198f8004e3f96426d606fce4d69-folder-structure/backend/repositories/product_catalog/product_repository.py
+++ 7391c198f8004e3f96426d606fce4d69-folder-structure/backend/repositories/product_catalog/product_repository.py
@@ -1,24 +1,10 @@
 from product_catalog_management.models import Product, Category
 from product_catalog_management.extensions import db
-from sqlalchemy import or_
 
 class ProductRepository:
     def get_product_by_id(self, product_id: int) -> Product:
         return Product.query.get(product_id)
 
-    def get_categories_by_ids(self, category_ids: list) -> list:
-        return Category.query.filter(Category.id.in_(category_ids)).all()
-
     def delete_product(self, product: Product) -> None:
         product.is_active = False
         db.session.commit()
-
-    def search_products(self, search_term: str, page: int, per_page: int):
-        return Product.query.filter(
-            Product.is_active,
-            or_(
-                Product.name.like(f'%{search_term}%'),
-                Product.description.like(f'%{search_term}%'),
-                Product.categories.any(Category.name.like(f'%{search_term}%'))
-            )
-        ).paginate(page, per_page, False)
