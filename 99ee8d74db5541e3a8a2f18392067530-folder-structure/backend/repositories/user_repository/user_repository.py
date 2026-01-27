bea42a66bd29499ea26d42d90fcd3cac4afe20ab
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 14:48:56 +0530

    commited by n8n

---
+++ 99ee8d74db5541e3a8a2f18392067530-folder-structure/backend/repositories/user_repository/user_repository.py
@@ -0,0 +1,14 @@
+# Epic Title: User Account Management
+
+from backend.database import db_session
+from backend.models.user import User
+
+class UserRepository:
+    def create_user(self, email: str, password: str) -> User:
+        new_user = User(email=email, password=password)
+        db_session.add(new_user)
+        db_session.commit()
+        return new_user
+
+    def get_user_by_email(self, email: str) -> User:
+        return db_session.query(User).filter_by(email=email).first()
