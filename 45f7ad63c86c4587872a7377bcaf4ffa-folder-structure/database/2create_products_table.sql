ec05ecb63ca87fa750136e7c6bed6894c25b42fb
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 14:31:03 +0530

    commited by n8n

--- 45f7ad63c86c4587872a7377bcaf4ffa-folder-structure/database/2create_products_table.sql
+++ 45f7ad63c86c4587872a7377bcaf4ffa-folder-structure/database/2create_products_table.sql
@@ -3,9 +3,8 @@
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(200) NOT NULL UNIQUE,
-    category_id INT NOT NULL,
+    category VARCHAR(200) NOT NULL,
     attributes TEXT NOT NULL,
     price FLOAT NOT NULL,
-    description TEXT NOT NULL,
-    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(id)
+    description TEXT NOT NULL
 );
