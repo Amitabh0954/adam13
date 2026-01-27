7c4788f0a2c259955f5bb825ab3f5659b63f0956
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-27 15:22:09 +0530

    commited by n8n

--- e36e6768693243e7a46d2028a2f7973d-folder-structure/backend/repositories/account/account_repository.py
+++ e36e6768693243e7a46d2028a2f7973d-folder-structure/backend/repositories/account/account_repository.py
@@ -7,8 +7,15 @@ class AccountRepository:
     def get_user_by_email(self, email: str) -> User:
         return db_session.query(User).filter_by(email=email).first()
 
+    def get_user_by_id(self, user_id: int) -> User:
+        return db_session.query(User).filter_by(id=user_id).first()
+
     def create_user(self, email: str, password: str) -> User:
         new_user = User(email=email, password=password)
         db_session.add(new_user)
         db_session.commit()
         return new_user
+
+    def update_user(self, user: User):
+        db_session.add(user)
+        db_session.commit()
