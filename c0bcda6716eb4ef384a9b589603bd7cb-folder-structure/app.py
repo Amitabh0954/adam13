f0499819b23bc02a45a6af8ab86d3b3cb05436b0
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-29 17:12:54 +0530

    commited by n8n

--- c0bcda6716eb4ef384a9b589603bd7cb-folder-structure/app.py
+++ c0bcda6716eb4ef384a9b589603bd7cb-folder-structure/app.py
@@ -3,7 +3,6 @@
 from flask import Flask
 from structured_logging import setup_logging
 from backend.product_catalog_management.controllers.product_controller import product_blueprint
-from backend.product_catalog_management.controllers.category_controller import category_blueprint
 
 def create_app() -> Flask:
     app = Flask(__name__)
@@ -11,18 +10,15 @@ def create_app() -> Flask:
     setup_logging()
     
     app.register_blueprint(product_blueprint, url_prefix='/api/v1')
-    app.register_blueprint(category_blueprint, url_prefix='/api/v1')
     
     @app.before_first_request
     def initialize_database():
         from backend.product_catalog_management.models.product import Base as ProductBase
-        from backend.product_catalog_management.models.category import Base as CategoryBase
         from sqlalchemy import create_engine
         from sqlalchemy.orm import sessionmaker
         
         engine = create_engine('mysql+pymysql://user:password@localhost/dbname')
         ProductBase.metadata.create_all(engine)
-        CategoryBase.metadata.create_all(engine)
         session = sessionmaker(bind=engine)()
         app.config['db_session'] = session
     
