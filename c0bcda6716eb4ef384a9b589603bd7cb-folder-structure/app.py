fedc57b5f779a091633520f8a4710ad634ea555f
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-29 17:07:51 +0530

    commited by n8n

---
+++ c0bcda6716eb4ef384a9b589603bd7cb-folder-structure/app.py
@@ -0,0 +1,34 @@
+# Epic Title: User Account Management
+
+from flask import Flask
+from structured_logging import setup_logging
+from backend.user_account_management.controllers.user_controller import user_blueprint
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    
+    setup_logging()
+    
+    app.register_blueprint(user_blueprint, url_prefix='/api/v1')
+    
+    @app.before_first_request
+    def initialize_database():
+        from backend.user_account_management.models.user import Base
+        from sqlalchemy import create_engine
+        from sqlalchemy.orm import sessionmaker
+        
+        engine = create_engine('mysql+pymysql://user:password@localhost/dbname')
+        Base.metadata.create_all(engine)
+        session = sessionmaker(bind=engine)()
+        app.config['db_session'] = session
+    
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        session = app.config['db_session']
+        session.remove()
+    
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000, debug=True)
