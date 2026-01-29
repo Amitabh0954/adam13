a603ab4ede820935f4383814a37c1a017752c4a1
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-29 17:12:59 +0530

    commited by n8n

--- c0bcda6716eb4ef384a9b589603bd7cb-folder-structure/app.py
+++ c0bcda6716eb4ef384a9b589603bd7cb-folder-structure/app.py
@@ -2,27 +2,25 @@
 
 from flask import Flask
 from structured_logging import setup_logging
-from backend.user_account_management.controllers.password_reset_controller import password_reset_blueprint
+from backend.user_account_management.controllers.login_controller import login_blueprint
 
 def create_app() -> Flask:
     app = Flask(__name__)
     
     setup_logging()
     
-    app.register_blueprint(password_reset_blueprint, url_prefix='/api/v1')
+    app.register_blueprint(login_blueprint, url_prefix='/api/v1')
     
     @app.before_first_request
     def initialize_database():
         from backend.user_account_management.models.user import Base as UserBase
         from backend.user_account_management.models.session import Base as SessionBase
-        from backend.user_account_management.models.password_reset import Base as PasswordResetBase
         from sqlalchemy import create_engine
         from sqlalchemy.orm import sessionmaker
         
         engine = create_engine('mysql+pymysql://user:password@localhost/dbname')
         UserBase.metadata.create_all(engine)
         SessionBase.metadata.create_all(engine)
-        PasswordResetBase.metadata.create_all(engine)
         session = sessionmaker(bind=engine)()
         app.config['db_session'] = session
     
