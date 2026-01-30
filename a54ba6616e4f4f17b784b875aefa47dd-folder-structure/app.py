9e14863f6780592808454f65e9f6eeb5cdaaeb8c
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 10:11:35 +0530

    commited by n8n

--- a54ba6616e4f4f17b784b875aefa47dd-folder-structure/app.py
+++ a54ba6616e4f4f17b784b875aefa47dd-folder-structure/app.py
@@ -1,13 +1,11 @@
 # Epic Title: Product Catalog Management
 from flask import Flask
-from datetime import timedelta
 from backend.user_account.routes.user_registration import user_registration_bp
 from backend.user_session.routes.user_login import user_login_bp
 from backend.user_account.routes.password_recovery import password_recovery_bp
 from backend.user_account.routes.profile_management import profile_management_bp
 from backend.product_catalog.routes.add_product import add_product_bp
 from backend.product_catalog.routes.update_product import update_product_bp
-from backend.product_catalog.routes.delete_product import delete_product_bp
 
 def create_app() -> Flask:
     app = Flask(__name__)
@@ -20,7 +18,6 @@ def create_app() -> Flask:
     app.register_blueprint(profile_management_bp, url_prefix='/api/user_account')
     app.register_blueprint(add_product_bp, url_prefix='/api/product_catalog')
     app.register_blueprint(update_product_bp, url_prefix='/api/product_catalog')
-    app.register_blueprint(delete_product_bp, url_prefix='/api/product_catalog')
 
     @app.route('/')
     def index():
