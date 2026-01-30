a73bc262f98faad79259a2499aa5f53f56702de0
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 15:00:09 +0530

    commited by n8n

---
+++ 37c0161884b44b01b662af423d499fc7-folder-structure/app.py
@@ -0,0 +1,12 @@
+# Epic Title: User Registration
+
+import os
+import django
+from django.conf import settings
+from django.core.management import execute_from_command_line
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+django.setup()
+
+if __name__ == "__main__":
+    execute_from_command_line()
