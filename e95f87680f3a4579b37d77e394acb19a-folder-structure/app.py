86fa9fa962acaf75050da0690ee0dd1dc39ee345
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 11:17:48 +0530

    commited by n8n

--- e95f87680f3a4579b37d77e394acb19a-folder-structure/app.py
+++ e95f87680f3a4579b37d77e394acb19a-folder-structure/app.py
@@ -4,7 +4,6 @@ from flask_login import LoginManager, UserMixin, login_user, logout_user, login_
 from datetime import timedelta
 from backend.services.user_account_management.register_service import RegisterService
 from backend.services.user_account_management.login_service import LoginService
-from backend.services.user_account_management.password_recovery_service import PasswordRecoveryService
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
@@ -15,7 +14,6 @@ login_manager.init_app(app)
 
 register_service = RegisterService()
 login_service = LoginService()
-password_recovery_service = PasswordRecoveryService()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -59,37 +57,6 @@ def login():
 def logout():
     logout_user()
     return jsonify({"message": "Logged out successfully"}), 200
-
-@app.route('/api/user_management/request-password-reset', methods=['POST'])
-def request_password_reset():
-    data = request.get_json()
-    email = data.get('email')
-
-    if not email:
-        return jsonify({"error": "Email is required"}), 400
-
-    response = password_recovery_service.initiate_password_reset(email)
-
-    if response.get("error"):
-        return jsonify(response), 400
-
-    return jsonify(response), 200
-
-@app.route('/api/user_management/reset-password', methods=['POST'])
-def reset_password():
-    data = request.get_json()
-    token = data.get('token')
-    new_password = data.get('new_password')
-
-    if not token or not new_password:
-        return jsonify({"error": "Token and new password are required"}), 400
-
-    response = password_recovery_service.reset_password(token, new_password)
-
-    if response.get("error"):
-        return jsonify(response), 400
-
-    return jsonify(response), 200
 
 @app.route('/')
 def index():
