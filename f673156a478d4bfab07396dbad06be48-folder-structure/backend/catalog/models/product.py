bfa66c8d3bf77ad6a3289d73c96b6080aaa1f18a
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 15:27:12 +0530

    commited by n8n

---
+++ f673156a478d4bfab07396dbad06be48-folder-structure/backend/catalog/models/product.py
@@ -0,0 +1,13 @@
+# Epic Title: Add New Product
+
+from django.db import models
+from catalog.models.category import Category
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self) -> str:
+        return self.name
