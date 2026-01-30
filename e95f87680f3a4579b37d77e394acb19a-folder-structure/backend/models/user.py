0b424de938e4bdf25d8bec31e54267da0d9db9c4
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 11:12:09 +0530

    commited by n8n

---
+++ e95f87680f3a4579b37d77e394acb19a-folder-structure/backend/models/user.py
@@ -0,0 +1,16 @@
+# Epic Title: User Account Management
+from flask_login import UserMixin
+
+class User(UserMixin):
+    def __init__(self, id: int, email: str):
+        self.id = id
+        self.email = email
+
+    @staticmethod
+    def get(user_id: int):
+        from repositories.user_repository import UserRepository
+        user_repository = UserRepository()
+        user_data = user_repository.get_user_by_id(user_id)
+        if user_data:
+            return User(user_data['id'], user_data['email'])
+        return None
