649da2c6d3b7213dc449a941f427a32a6375f119
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 15:14:13 +0530

    commited by n8n

--- c638eab265dc4750a233d3a4e6bebeb0-folder-structure/backend/cart/services/cart_service.py
+++ c638eab265dc4750a233d3a4e6bebeb0-folder-structure/backend/cart/services/cart_service.py
@@ -1,10 +1,10 @@
-# Epic Title: Modify Quantity of Products in Shopping Cart
+# Epic Title: Save Shopping Cart for Logged-in Users
 
 from cart.repositories.cart_repository import CartRepository
 from cart.models.cart import Cart, CartItem
 from products.models.product import Product
 from django.contrib.auth.models import User
-from typing import Optional
+from typing import Optional, List
 
 class CartService:
     def __init__(self):
@@ -21,7 +21,7 @@ class CartService:
         product = Product.objects.get(id=product_id)
         return self.cart_repository.add_cart_item(cart, product, quantity)
 
-    def get_cart_items(self, user: Optional[User]) -> Cart:
+    def get_cart_items(self, user: Optional[User]) -> List[CartItem]:
         cart = self.get_or_create_cart(user)
         return self.cart_repository.get_cart_items(cart)
 
