881bd46bf09007dab5c10faa2c4ae52240f1276e
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 15:10:50 +0530

    commited by n8n

---
+++ c638eab265dc4750a233d3a4e6bebeb0-folder-structure/backend/users/models/user.py
@@ -0,0 +1,9 @@
+# Epic Title: User Registration
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    def __str__(self) -> str:
+        return self.username
