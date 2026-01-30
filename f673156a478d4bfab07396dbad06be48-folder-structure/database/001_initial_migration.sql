10c49b0142e00f9af7c50e0490b59e4d23aa1ca3
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 15:25:47 +0530

    commited by n8n

---
+++ f673156a478d4bfab07396dbad06be48-folder-structure/database/001_initial_migration.sql
@@ -0,0 +1,4 @@
+# Epic Title: User Registration
+
+-- This initial migration script is to setup the database with the necessary tables.
+-- Django's built-in authentication system will handle the user-related tables.
