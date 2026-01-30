3f7c7440b1bfef4642c42044d42811e17aff42ca
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 11:17:21 +0530

    commited by n8n

--- e95f87680f3a4579b37d77e394acb19a-folder-structure/database/product_db.sql
+++ e95f87680f3a4579b37d77e394acb19a-folder-structure/database/product_db.sql
@@ -3,20 +3,12 @@
 CREATE DATABASE IF NOT EXISTS product_db;
 USE product_db;
 
-CREATE TABLE IF NOT EXISTS categories (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    parent_id INT,
-    FOREIGN KEY (parent_id) REFERENCES categories(id)
-);
-
 CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     description TEXT NOT NULL,
     price FLOAT NOT NULL CHECK(price > 0),
-    category_id INT NOT NULL,
+    category VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FULLTEXT(name, description),
-    FOREIGN KEY (category_id) REFERENCES categories(id)
+    FULLTEXT(name, description, category)
 );
