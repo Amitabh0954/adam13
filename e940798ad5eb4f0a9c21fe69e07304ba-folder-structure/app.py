73b8afbaaa6c91f41889dcac34ae09b57d0453b1
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 11:26:06 +0530

    commited by n8n

--- e940798ad5eb4f0a9c21fe69e07304ba-folder-structure/app.py
+++ e940798ad5eb4f0a9c21fe69e07304ba-folder-structure/app.py
@@ -3,7 +3,6 @@ from flask import Flask
 from backend.controllers.user_account.registration_controller import registration_bp
 from backend.controllers.user_account.login_controller import login_bp
 from backend.controllers.user_account.password_reset_controller import password_reset_bp
-from backend.controllers.user_account.profile_controller import profile_bp
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
@@ -11,7 +10,6 @@ app.config['SECRET_KEY'] = 'supersecretkey'
 app.register_blueprint(registration_bp)
 app.register_blueprint(login_bp)
 app.register_blueprint(password_reset_bp)
-app.register_blueprint(profile_bp)
 
 @app.route('/')
 def index():
