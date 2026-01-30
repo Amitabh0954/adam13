d2f1dffc1b594129dcb494e08e45fe65e0d76f56
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 15:22:52 +0530

    commited by n8n

--- 708d674bf42943ff8eee0b493a6a1448-folder-structure/app.py
+++ 708d674bf42943ff8eee0b493a6a1448-folder-structure/app.py
@@ -1,4 +1,4 @@
-# Epic Title: User Login
+# Epic Title: Update Product Details
 
 import os
 import django
@@ -7,6 +7,9 @@ from django.conf import settings
 from django.urls import path
 from django.apps import apps
 from account.controllers.user_controller import register, login_user, logout_user
+from account.controllers.password_reset_controller import request_password_reset, reset_password
+from account.controllers.profile_controller import update_profile
+from catalog.controllers.product_controller import add_product, update_product
 
 settings.configure(
     DEBUG=os.getenv('DEBUG', 'on') == 'on',
@@ -14,6 +17,7 @@ settings.configure(
         'django.contrib.auth',
         'django.contrib.contenttypes',
         'account',
+        'catalog',
     ],
     MIDDLEWARE=[
         'django.middleware.common.CommonMiddleware',
@@ -32,6 +36,8 @@ settings.configure(
             'PORT': os.getenv('DATABASE_PORT', '3306'),
         }
     },
+    FRONTEND_URL=os.getenv('FRONTEND_URL', 'http://localhost:3000'),
+    DEFAULT_FROM_EMAIL=os.getenv('DEFAULT_FROM_EMAIL', 'noreply@example.com'),
 )
 
 apps.populate(settings.INSTALLED_APPS)
@@ -40,6 +46,11 @@ urlpatterns = [
     path('register/', register),
     path('login/', login_user),
     path('logout/', logout_user),
+    path('request-password-reset/', request_password_reset),
+    path('reset-password/', reset_password),
+    path('update-profile/', update_profile),
+    path('add-product/', add_product),
+    path('update-product/<int:product_id>/', update_product),
 ]
 
 if __name__ == '__main__':
