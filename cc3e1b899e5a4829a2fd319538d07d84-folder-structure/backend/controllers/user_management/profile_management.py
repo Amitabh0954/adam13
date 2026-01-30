c2ab13f732866a43cb9a1e1df6c0db8895d523be
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 10:23:23 +0530

    commited by n8n

---
+++ cc3e1b899e5a4829a2fd319538d07d84-folder-structure/backend/controllers/user_management/profile_management.py
@@ -0,0 +1,24 @@
+# Epic Title: User Account Management
+from flask import Blueprint, request, jsonify
+from flask_login import current_user, login_required
+from services.user_management.profile_management_service import ProfileManagementService
+
+profile_management_bp = Blueprint('profile_management', __name__)
+profile_management_service = ProfileManagementService()
+
+@profile_management_bp.route('/profile', methods=['GET'])
+@login_required
+def get_profile():
+    response = profile_management_service.get_profile(current_user.id)
+    return jsonify(response), 200
+
+@profile_management_bp.route('/profile', methods=['PUT'])
+@login_required
+def update_profile():
+    data = request.get_json()
+    response = profile_management_service.update_profile(current_user.id, data)
+
+    if response.get("error"):
+        return jsonify(response), 400
+
+    return jsonify(response), 200
