b637954b38b8e6d98ebd9974c8a2ddb38408e767
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 09:59:46 +0530

    commited by n8n

--- 5c3c45fe83574f1486f3205e144ab28e-folder-structure/app.py
+++ 5c3c45fe83574f1486f3205e144ab28e-folder-structure/app.py
@@ -6,7 +6,6 @@ from backend.product_catalog.routes.delete_product import delete_product_bp
 from backend.product_catalog.routes.search_product import search_product_bp
 from backend.product_catalog.routes.category_management import category_management_bp
 from backend.shopping_cart.routes.add_to_cart import add_to_cart_bp
-from backend.shopping_cart.routes.remove_from_cart import remove_from_cart_bp
 
 def create_app() -> Flask:
     app = Flask(__name__)
@@ -19,7 +18,6 @@ def create_app() -> Flask:
     app.register_blueprint(search_product_bp, url_prefix='/api/product_catalog')
     app.register_blueprint(category_management_bp, url_prefix='/api/product_catalog')
     app.register_blueprint(add_to_cart_bp, url_prefix='/api/shopping_cart')
-    app.register_blueprint(remove_from_cart_bp, url_prefix='/api/shopping_cart')
 
     @app.route('/')
     def index():
