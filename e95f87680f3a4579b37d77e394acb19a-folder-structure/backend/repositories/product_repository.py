60acfc8f7efed5d9b510d7f58bfcbaf5fd7ee7fd
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 11:17:31 +0530

    commited by n8n

--- e95f87680f3a4579b37d77e394acb19a-folder-structure/backend/repositories/product_repository.py
+++ e95f87680f3a4579b37d77e394acb19a-folder-structure/backend/repositories/product_repository.py
@@ -33,9 +33,3 @@ class ProductRepository:
         self.cursor.execute(sql, params)
         self.connection.commit()
         return self.cursor.rowcount > 0
-
-    def delete_product(self, product_id: int) -> bool:
-        query = "DELETE FROM products WHERE id = %s"
-        self.cursor.execute(query, (product_id,))
-        self.connection.commit()
-        return self.cursor.rowcount > 0
