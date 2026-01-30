53185ce33d4cf13a5a10904bfa38a7dc167bb772
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 11:17:44 +0530

    commited by n8n

--- e95f87680f3a4579b37d77e394acb19a-folder-structure/app.py
+++ e95f87680f3a4579b37d77e394acb19a-folder-structure/app.py
@@ -1,11 +1,10 @@
 # Epic Title: User Account Management
 from flask import Flask, request, jsonify
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 from datetime import timedelta
 from backend.services.user_account_management.register_service import RegisterService
 from backend.services.user_account_management.login_service import LoginService
 from backend.services.user_account_management.password_recovery_service import PasswordRecoveryService
-from backend.services.user_account_management.profile_management_service import ProfileManagementService
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
@@ -17,7 +16,6 @@ login_manager.init_app(app)
 register_service = RegisterService()
 login_service = LoginService()
 password_recovery_service = PasswordRecoveryService()
-profile_management_service = ProfileManagementService()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -87,23 +85,6 @@ def reset_password():
         return jsonify({"error": "Token and new password are required"}), 400
 
     response = password_recovery_service.reset_password(token, new_password)
-
-    if response.get("error"):
-        return jsonify(response), 400
-
-    return jsonify(response), 200
-
-@app.route('/api/user_management/profile', methods=['GET'])
-@login_required
-def get_profile():
-    response = profile_management_service.get_profile(current_user.id)
-    return jsonify(response), 200
-
-@app.route('/api/user_management/profile', methods=['PUT'])
-@login_required
-def update_profile():
-    data = request.get_json()
-    response = profile_management_service.update_profile(current_user.id, data)
 
     if response.get("error"):
         return jsonify(response), 400
