51a72add79fa9b958dba2c421d29f494c3446c35
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 11:16:58 +0530

    commited by n8n

--- e95f87680f3a4579b37d77e394acb19a-folder-structure/backend/repositories/shopping_cart_repository.py
+++ e95f87680f3a4579b37d77e394acb19a-folder-structure/backend/repositories/shopping_cart_repository.py
@@ -38,3 +38,28 @@ class ShoppingCartRepository:
         self.cursor.execute(query, (quantity, cart_id, product_id))
         self.connection.commit()
         return self.cursor.rowcount > 0
+
+    def delete_cart_item(self, cart_id: int, product_id: int) -> bool:
+        query = "DELETE FROM cart_items WHERE cart_id = %s AND product_id = %s"
+        self.cursor.execute(query, (cart_id, product_id))
+        self.connection.commit()
+        return self.cursor.rowcount > 0
+
+    def save_cart_state(self, user_id: int, cart_items: list) -> bool:
+        cart = self.get_cart_by_user_id(user_id)
+        if cart:
+            cart_id = cart['id']
+        else:
+            cart_id = self.create_cart(user_id)
+
+        for item in cart_items:
+            self.add_cart_item(cart_id, item['product_id'], item['quantity'])
+        return True
+
+    def retrieve_cart_state(self, user_id: int) -> list:
+        cart = self.get_cart_by_user_id(user_id)
+        if not cart:
+            return []
+
+        cart_id = cart['id']
+        return self.get_cart_items(cart_id)
