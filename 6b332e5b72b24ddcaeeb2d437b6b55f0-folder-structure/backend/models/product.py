742207b7d36f662c728b370cfbb27d0621af9aae
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 15:45:20 +0530

    commited by n8n

--- 6b332e5b72b24ddcaeeb2d437b6b55f0-folder-structure/backend/models/product.py
+++ 6b332e5b72b24ddcaeeb2d437b6b55f0-folder-structure/backend/models/product.py
@@ -1,11 +1,13 @@
-# Epic Title: Add New Product
+# Epic Title: Product Categorization
 
 from django.db import models
+from backend.models.category import Category
 
 class Product(models.Model):
     name = models.CharField(max_length=255, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
+    categories = models.ManyToManyField(Category)
 
     def __str__(self) -> str:
         return self.name
