c4af83ae649aad33a7bde5d4e7567ed63c28b0cc
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 10:15:47 +0530

    commited by n8n

--- 0e179a0721884a9ab0a6bb793dea4bc6-folder-structure/app.py
+++ 0e179a0721884a9ab0a6bb793dea4bc6-folder-structure/app.py
@@ -8,7 +8,6 @@ from backend.user_account.routes.profile_management import profile_management_bp
 from backend.product_catalog.routes.add_product import add_product_bp
 from backend.product_catalog.routes.update_product import update_product_bp
 from backend.product_catalog.routes.delete_product import delete_product_bp
-from backend.product_catalog.routes.search_product import search_product_bp
 from flask_login import LoginManager
 
 def create_app() -> Flask:
@@ -26,7 +25,6 @@ def create_app() -> Flask:
     app.register_blueprint(add_product_bp, url_prefix='/api/product_catalog')
     app.register_blueprint(update_product_bp, url_prefix='/api/product_catalog')
     app.register_blueprint(delete_product_bp, url_prefix='/api/product_catalog')
-    app.register_blueprint(search_product_bp, url_prefix='/api/product_catalog')
 
     @app.route('/')
     def index():
