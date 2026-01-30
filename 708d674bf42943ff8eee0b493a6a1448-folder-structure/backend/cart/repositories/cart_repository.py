0d36a96218f6cce92518be7d58b910dbbbf36620
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 15:26:18 +0530

    commited by n8n

--- 708d674bf42943ff8eee0b493a6a1448-folder-structure/backend/cart/repositories/cart_repository.py
+++ 708d674bf42943ff8eee0b493a6a1448-folder-structure/backend/cart/repositories/cart_repository.py
@@ -1,9 +1,9 @@
-# Epic Title: Modify Quantity of Products in Shopping Cart
+# Epic Title: Save Shopping Cart for Logged-in Users
 
 from cart.models.cart import Cart, CartItem
 from catalog.models.product import Product
 from django.contrib.auth.models import User
-from typing import Optional
+from typing import Optional, List
 
 class CartRepository:
 
@@ -44,3 +44,6 @@ class CartRepository:
             return True
         except CartItem.DoesNotExist:
             return False
+
+    def get_cart_items(self, cart: Cart) -> List[CartItem]:
+        return list(CartItem.objects.filter(cart=cart))
