0f71872550769ee046181e0a0f60330d2b2986b2
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 15:26:25 +0530

    commited by n8n

--- 708d674bf42943ff8eee0b493a6a1448-folder-structure/app.py
+++ 708d674bf42943ff8eee0b493a6a1448-folder-structure/app.py
@@ -1,4 +1,4 @@
-# Epic Title: Save Shopping Cart for Logged-in Users
+# Epic Title: Add Product to Shopping Cart
 
 import os
 import django
@@ -12,7 +12,7 @@ from account.controllers.profile_controller import update_profile
 from catalog.controllers.product_controller import add_product, update_product, delete_product
 from catalog.controllers.search_controller import search_products
 from catalog.controllers.category_controller import add_category
-from cart.controllers.cart_controller import add_product_to_cart, remove_product_from_cart, modify_quantity_in_cart, save_cart_state
+from cart.controllers.cart_controller import add_product_to_cart
 
 settings.configure(
     DEBUG=os.getenv('DEBUG', 'on') == 'on',
@@ -59,9 +59,6 @@ urlpatterns = [
     path('search-products/', search_products),
     path('add-category/', add_category),
     path('add-product-to-cart/', add_product_to_cart),
-    path('remove-product-from-cart/', remove_product_from_cart),
-    path('modify-quantity-in-cart/', modify_quantity_in_cart),
-    path('save-cart-state/', save_cart_state),
 ]
 
 if __name__ == '__main__':
