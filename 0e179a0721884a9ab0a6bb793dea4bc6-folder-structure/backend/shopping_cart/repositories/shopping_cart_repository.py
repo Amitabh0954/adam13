b1039b637c547f3688d68613a332086f073a4f7d
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 10:15:28 +0530

    commited by n8n

--- 0e179a0721884a9ab0a6bb793dea4bc6-folder-structure/backend/shopping_cart/repositories/shopping_cart_repository.py
+++ 0e179a0721884a9ab0a6bb793dea4bc6-folder-structure/backend/shopping_cart/repositories/shopping_cart_repository.py
@@ -11,20 +11,8 @@ class ShoppingCartRepository:
         )
         self.cursor = self.connection.cursor(dictionary=True)
 
-    def save_cart(self, user_id: int) -> bool:
-        query_temp = "DELETE FROM saved_cart_items WHERE user_id = %s"
-        self.cursor.execute(query_temp, (user_id,))
-        query = """
-            INSERT INTO saved_cart_items (user_id, product_id, quantity)
-            SELECT user_id, product_id, quantity FROM cart_items WHERE user_id = %s
-        """
-        self.cursor.execute(query, (user_id,))
+    def modify_quantity(self, user_id: int, cart_item_id: int, quantity: int) -> bool:
+        query = "UPDATE cart_items SET quantity = %s WHERE id = %s AND user_id = %s"
+        self.cursor.execute(query, (quantity, cart_item_id, user_id))
         self.connection.commit()
         return self.cursor.rowcount > 0
-
-    def retrieve_cart(self, user_id: int) -> list:
-        query = """
-            SELECT product_id, quantity FROM saved_cart_items WHERE user_id = %s
-        """
-        self.cursor.execute(query, (user_id,))
-        return self.cursor.fetchall()
