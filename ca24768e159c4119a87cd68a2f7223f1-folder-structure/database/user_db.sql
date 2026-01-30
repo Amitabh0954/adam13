2ee405876a07a982bdac3fe3827702811dfcbdb3
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2026-01-30 11:33:45 +0530

    commited by n8n

--- ca24768e159c4119a87cd68a2f7223f1-folder-structure/database/user_db.sql
+++ ca24768e159c4119a87cd68a2f7223f1-folder-structure/database/user_db.sql
@@ -6,7 +6,14 @@ USE user_db;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS password_resets (
+    user_id INT NOT NULL,
+    reset_token VARCHAR(255) NOT NULL UNIQUE,
+    expires_at DATETIME NOT NULL,
+    PRIMARY KEY (reset_token),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
